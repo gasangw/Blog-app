763b19fa6f2dc7cc6c959b9aaf89bb7bd1bf7c19
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-09-28 22:28:37 +0200

    Add methods in different models

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
-    belongs_to :post
-    belongs_to :user
+  belongs_to :post
+  belongs_to :user
+
+  def likes_counter
+    post.increment!(:LikesCounter)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,13 @@
 class Post < ApplicationRecord
-    belongs_to :user
-    has_many :likes
-    has_many :comments
+  belongs_to :user
+  has_many :likes
+  has_many :comments
+
+  def recent_comments
+    Comment.limit(5).order(created_at: :desc)
+  end
+
+  def post_counter
+    user.increment!(:PostsCounter)
+  end
 end
