0a5bf7a72085e9d993eae4b8cc3a6e5dc30c9f2e
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-09-29 15:22:26 +0200

    Add validations in user.rb file and post.rb file

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  validates :Name, presence: true
+  validates :PostsCounter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   def recent_posts
     Post.limit(3).order(created_at: :desc)
   end
