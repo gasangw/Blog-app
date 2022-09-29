19b7e60e47abefbecafdbf8bdfd7024670e31cb7
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-09-29 08:27:03 +0200

    Add changes required by the reviewer

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,11 @@ class Like < ApplicationRecord
   belongs_to :post
   belongs_to :user
 
+  after_save :likes_counter
+
   def likes_counter
     post.increment!(:LikesCounter)
   end
+
+  private :likes_counter
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  after_save :post_counter
+
   def recent_comments
     Comment.limit(5).order(created_at: :desc)
   end
@@ -10,4 +12,6 @@ class Post < ApplicationRecord
   def post_counter
     user.increment!(:PostsCounter)
   end
+
+  private :post_counter
 end
