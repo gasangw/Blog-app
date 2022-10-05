95250ed933330ee78657d960a9bd78b92e057d4a
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-10-05 14:24:25 +0200

    Add methods for listing the users and the showing them in the controller

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @user = User.all
+  end
 
-  def show; end
+  def show
+    @user = User.find(params[:id].to_i)  
+  end
 end
