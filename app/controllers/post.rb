385664cef566aad05185ed13a4b67825aefeb5f3
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-10-05 16:26:57 +0200

    add methods in controllers

--- app/controllers/post.rb
+++ app/controllers/post.rb
@@ -1,7 +1,11 @@
 class PostsController < ApplicationController
   def index
-    @user = User.includes(posts: [:comments]).find(params[:user_id].to_i)
+    @user = User.find(params[:user_id].to_i)
+    @post = @user.posts
   end
 
-  def show; end
+  def show
+    @post = Post.joins(:user).where(user: { id: params[:user_id] }).find(params[:id])
+    @comments = @post.comments
+  end
 end
