5177afd310bb70156e560cf7812e65d4f8ea7763
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-10-07 15:15:36 +0200

    Solve linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @user = @post.user,
-    @comments = @post.comments
+            @comments = @post.comments
   end
 
   def create
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  
+
   after_save :comment_counter
   validates :Text, presence: true
-   
+
   def comment_counter
     post.increment!(:CommentsCounter)
   end
