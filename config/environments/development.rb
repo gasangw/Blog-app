f3dc93f1b5129175dcfb46858802de5fd63c13eb
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-10-12 15:00:45 +0200

    Set up the enviroment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,8 +63,13 @@ Rails.application.configure do
   # config.i18n.raise_on_missing_translations = true
 
   # Annotate rendered view with file names.
-  # config.action_view.annotate_rendered_view_with_filenames = true
-
+ 
+ # config.action_view.annotate_rendered_view_with_filenames = true
+  config.action_mailer.raise_delivery_errors = false
+  
+  config.action_mailer.perform_caching = false
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
