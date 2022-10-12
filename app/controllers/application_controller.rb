eb3642acd263673d5bf167bf1b7107e880a0c13c
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-10-12 17:13:54 +0200

    correct the error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,22 @@
 class ApplicationController < ActionController::Base
+  # before_action :authenticate_user!
+  # protect_from_forgery with: :exception
+  # before_action :update_allowed_parameters, if: :devise_controller?
+
+  # protected
+
+  # def update_allowed_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:Name, :Bio, :Photo, :email, :password) }
+  #   devise_parameter_sanitizer.permit(:account_update) do |u|
+  #     u.permit(:Name, :Bio, :Photo, :email, :password, :current_password)
+  #   end
+  # end
   before_action :authenticate_user!
-  protect_from_forgery with: :exception
-  before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
-  def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:Name, :Bio, :Photo, :email, :password) }
-    devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:Name, :Bio, :Photo, :email, :password, :current_password)
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name password password_confirmation])
   end
 end
