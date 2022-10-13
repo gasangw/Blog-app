e18e063156fbd6df28c06a8c2ab41a1ddfd0161f
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-10-13 16:21:56 +0200

    Solve linter errors

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,12 +1,16 @@
 class Ability
   include CanCan::Ability
-   user ||= User.new # guest user (not logged in)
+
+  def initialize(user)
+    user ||= User.new
+
     if user.role == 'admin'
       can :manage, :all
     else
       can %i[read create], :all
-      can %i[update destroy], Post, user_id: user.id
-      can %i[update destroy], Comment, user_id: user.id
+      can %i[update destroy], Post, author_id: user.id
+      can %i[update destroy], Comment, author_id: user.id
+
     end
   end
 end
