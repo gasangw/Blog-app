60f84dc1c010e6223da2cca733130c8d8e8b8664
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-10-13 16:10:51 +0200

    add a route to help destroy session as one sign out

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,16 +4,6 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-  #   user ||= User.new # guest user (not logged in)
-  #   if user.role == 'admin'
-  #     can :manage, :all
-  #   else
-  #     can %i[read create], :all
-  #     can %i[update destroy], Post, user_id: user.id
-  #     can %i[update destroy], Comment, user_id: user.id
-  #   end
-  #   #
-  #   # See the wiki for details:
-  #   # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
-  # end
+ 
+  end
 end
