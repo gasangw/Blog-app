689588749ee19304aff9887f5ff418bf9014e944
Elisha Tetteyfio <tetteyfioelisha@gmail.com>
2022-10-13 14:14:00 +0000

    Add ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,12 @@
 class Ability
   include CanCan::Ability
+   user ||= User.new # guest user (not logged in)
+    if user.role == 'admin'
+      can :manage, :all
+    else
+      can %i[read create], :all
+      can %i[update destroy], Post, user_id: user.id
+      can %i[update destroy], Comment, user_id: user.id
+    end
+  end
 end
