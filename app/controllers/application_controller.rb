70c18ca8e857a4b3c61f8eb3d492d0b0c83e65a6
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-10-14 15:18:41 +0000

    Add API endpoints for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
-class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-  protect_from_forgery with: :exception
-  before_action :update_allowed_parameters, if: :devise_controller?
+class ApplicationController < ActionController::API
+  # before_action :authenticate_user!
+  # protect_from_forgery with: :exception
+  # before_action :update_allowed_parameters, if: :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, alert: exception.message
